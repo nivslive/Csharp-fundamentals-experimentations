d27223e604d791cebe9de6932fe590dc51d7e631
Nivs <nivsoficial@gmail.com>
2023-07-29 15:13:32 -0300

    calculator rules

---
+++ Calculator/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Calculator
+{
+	public class Functions
+	{
+        readonly int number1;
+        readonly int number2;
+
+        public Functions(int number1, int number2)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+        }
+		public int Add()
+		{
+			return this.number1 + this.number2;
+        }
+
+		public int Sub()
+		{
+            return this.number1 - this.number2;
+        }
+
+        public int Mult()
+        {
+            return this.number1 * this.number2;
+        }
+    }
+}
+
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void ResultValuePrint(int value)
+{
+    Console.WriteLine("O valor deu:" + value);
+}
+static int intParser(string data)
+{
+    if (int.TryParse(data, out int number))
+    {
+        return int.Parse(data);
+    }
+    return 0;
+}
+Console.WriteLine("Input the first number:");
+int number1 = intParser(Console.ReadLine() ?? "");
+Console.WriteLine("Input the second number:");
+int number2 = intParser(Console.ReadLine() ?? "");
 
+Calculator.Functions calculator = new(number1, number2);
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd Numbers!");
+Console.WriteLine("[S]ubtract Numbers!");
+Console.WriteLine("[M]ultiply Numbers!");
+string action = Console.ReadLine() ?? "E";
+int result;
+if(action == "A")
+{
+    result = calculator.Add();
+}
+else if (action == "M")
+{
+    result = calculator.Mult();
+}
+else if (action == "S")
+{
+    result = calculator.Sub();
+}
+else if (action == "E")
+{
+    result = 0;
+}
+else {
+    result = 0;
+}
+
+ResultValuePrint(result);
