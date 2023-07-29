fce0860d8cbf0b60a9df6acaf0e4c37013a8ee4c
Nivs <nivsoficial@gmail.com>
2023-07-29 19:03:41 -0300

    loops training

---
+++ Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 0;
+string @string = "test";
+while(number < 10)
+{
+    number++;
+    @string += "s";
+    Console.WriteLine($"Number is: {number}");
+    Console.WriteLine($"String is: {@string}");
+}
+
+Console.WriteLine("Loop is finished.");
+Console.WriteLine("Click on any button for end the program.");
+Console.ReadLine();
