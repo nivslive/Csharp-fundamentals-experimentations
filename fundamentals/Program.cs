967d8684ff037096630ff920067d56e034d91fcc
Nivs <nivsoficial@gmail.com>
2023-07-29 13:09:16 -0300

    methods

--- fundamentals/Program.cs
+++ fundamentals/Program.cs
@@ -1,22 +1,37 @@
-﻿Console.WriteLine("Hello!");
+﻿void PrintSelectionOption(string selectedOption)
+{
+    Console.WriteLine("Selected option:" + selectedOption);
+}
+
+
+Console.WriteLine("Hello!");
 Console.WriteLine("[S]ee all TODOs!");
 Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO!");
 Console.WriteLine("[E]xit!!");
 
+
 string userChoice = Console.ReadLine() ?? "";
 
-if (userChoice.Length >= 3)
+if (userChoice == "S")
 {
-    Console.WriteLine("Short Answer!");
-}
-else if (userChoice.Length < 10)
-{
-    Console.WriteLine("Medium Answer");
-}
-else {
-    Console.WriteLine("Short answer.");
+    PrintSelectionOption(userChoice);
 }
 
-Console.WriteLine("User input:" + userChoice);
+if (userChoice == "A")
+{
+    PrintSelectionOption(userChoice);
+}
+
+if (userChoice == "R")
+{
+    PrintSelectionOption(userChoice);
+}
+
+if (userChoice == "E")
+{
+    PrintSelectionOption(userChoice);
+}
+
+//Console.WriteLine("User input:" + userChoice);
 Console.ReadKey();
