992e29623c2593c001cd5dde7738fbfaa27a33e5
Nivs <nivsoficial@gmail.com>
2023-07-29 11:45:23 -0300

    first commit

---
+++ fundamentals/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs!");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO!");
+Console.WriteLine("[E]xit!!");
+
+string userChoice = Console.ReadLine() ?? "";
+bool isUserInputABC =
+    userChoice == "ABC" ||
+    userChoice == "test" ||
+    userChoice == "banana";
+if (userChoice.Length > 10)
+{
+    Console.WriteLine("IS ABC!");
+}
+Console.WriteLine("User input:" + userChoice);
+Console.ReadKey();
