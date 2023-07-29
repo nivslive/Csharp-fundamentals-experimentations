86033c6c45963a4ab88b9ae5ed4e81412662dbe0
Nivs <nivsoficial@gmail.com>
2023-07-29 13:57:03 -0300

    atts about classes integrations

---
+++ fundamentals/NumberExperimentations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace fundamentals
+{
+	public class NumberExperimentations
+	{
+		public static int AddNumberTestIfAboveThanTen(int a, int b)
+		{
+			if(a > 10)
+			{
+				return a + b;
+			}
+			return a - b;
+		}
+
+		public static bool IsLong(string input)
+		{
+			if(input.Length > 0)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
+
